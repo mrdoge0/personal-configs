15dbeacfd57956c7912a0b9ef9c3a61d3fbf39a6
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-07-13 20:06:36 +0000

    Update profile-dotfile.sh

--- profile-dotfile.sh
+++ profile-dotfile.sh
@@ -2,8 +2,8 @@
 ## PROFILE.D ENVIRONMENT MODDING
 ## 
 
-# Add the user's ~/.local/bin to PATH.
-export PATH="${PATH}:${HOME}/.local/bin"
+# Add the user's '~/.local/bin', and '/opt/personal-configs/bin' to PATH.
+export PATH="${PATH}:/opt/personal-configs/bin:${HOME}/.local/bin"
 
 # Export that I'm a nerd :D
 export EDITOR=nvim
@@ -11,4 +11,14 @@ export EDITOR=nvim
 # Set qt6ct as Qt5/6 theme provider if the DE is not KDE Plasma.
 if [ ! -f "/usr/share/wayland-sessions/*plasma*.desktop" ]; then
   export QT_QPA_PLATFORMTHEME=qt6ct
+fi
+
+# Mkdir the '~/.local/bin's for everyone
+if [ ! -d "${HOME}/.local/bin" ]; then
+  mkdir -p "${HOME}/.local/bin"
+fi
+
+# Install zshrc shim to everyone.
+if [ "$(cat ${HOME}/.zshrc)" -ne "source /opt/personal-configs/zshrc" ]; then
+  echo "source /opt/personal-configs/zshrc" > ${HOME}/.zshrc
 fi
