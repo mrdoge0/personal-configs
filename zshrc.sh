07261ff2f21b001e4435044e3288bff9106aa9f3
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2026-06-27 21:23:07 +0300

    fix syntax error

--- zshrc.sh
+++ zshrc.sh
@@ -46,7 +46,7 @@ for SC in '/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' '
 done
 
 # Source Zsh Autocomplete
-if [ "${NO_ZSH_AUTOCOMP}" == true ]; then
+if [ "${NO_ZSH_AUTOCOMP}" == 'true' ]; then
 	for SC in '/usr/share/zsh/site-functions/zsh-autocomplete/zsh-autocomplete.plugin.zsh'; do
 		[ -f "${SC}" ] && source "${SC}"
 	done
